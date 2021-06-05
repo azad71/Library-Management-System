9947b617b9f0d1a6670fc25e1198996a45366e49
azad71 <mdmamun732@gmail.com>
2021-06-05 23:54:45 +0600

    book route and view is done

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -23,8 +23,13 @@ exports.getBooks = async (req, res, next) => {
       count = await Book.find().countDocuments();
     }
 
-    // const categories = await Book.find().select({ category: 1, _id: 0 });
-    // console.log(categories);
+    let booksCountByCategory = await Book.aggregate([
+      { $group: { _id: { category: "$category" }, count: { $sum: 1 } } },
+    ]).replaceRoot({ category: { $concat: ["$_id.category"] }, count: "$count" });
+
+    let booksCountByAuthors = await Book.aggregate([
+      { $group: { _id: { author: "$author" }, count: { $sum: 1 } } },
+    ]).replaceRoot({ authorName: { $concat: ["$_id.author"] }, count: "$count" });
 
     res.render("books", {
       books: books,
@@ -33,93 +38,19 @@ exports.getBooks = async (req, res, next) => {
       user: req.session.user,
       limit,
       q,
+      categories: booksCountByCategory,
+      authors: booksCountByAuthors,
     });
   } catch (err) {
     console.log(err);
   }
 };
 
-// exports.getBooks = async (req, res, next) => {
-//   var page = req.params.page || 1;
-//   const filter = req.params.filter;
-//   const value = req.params.value;
-//   let searchObj = {};
-
-//   // constructing search object
-//   if (filter != "all" && value != "all") {
-//     // fetch books by search value and filter
-//     searchObj[filter] = value;
-//   }
-
-//   try {
-//     // Fetch books from database
-//     const books = await Book.find(searchObj)
-//       .skip(PER_PAGE * page - PER_PAGE)
-//       .limit(PER_PAGE);
-
-//     // Get the count of total available book of given filter
-//     const count = await Book.find(searchObj).countDocuments();
-
-//     res.render("books", {
-//       books: books,
-//       current: page,
-//       pages: Math.ceil(count / PER_PAGE),
-//       filter: filter,
-//       value: value,
-//       user: req.user,
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-exports.findBooks = async (req, res, next) => {
-  var page = req.params.page || 1;
-  const filter = req.body.filter.toLowerCase();
-  const value = req.body.searchName;
-
-  // show flash message if empty search field is sent to backend
-  if (value == "") {
-    req.flash("error", "Searc field is empty. Please fill the search field in order to get a result");
-    return res.redirect("back");
-  }
-
-  const searchObj = {};
-  searchObj[filter] = value;
-
-  try {
-    // Fetch books from database
-    const books = await Book.find(searchObj)
-      .skip(PER_PAGE * page - PER_PAGE)
-      .limit(PER_PAGE);
-
-    // Get the count of total available book of given filter
-    const count = await Book.find(searchObj).countDocuments();
-
-    res.render("books", {
-      books: books,
-      current: page,
-      pages: Math.ceil(count / PER_PAGE),
-      filter: filter,
-      value: value,
-      user: req.user,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-// find book details working procedure
-/*
-   1. fetch book from db by id
-   2. populate book with associated comments
-   3. render user/bookDetails template and send the fetched book
-*/
-
-exports.getBookDetails = async (req, res, next) => {
+exports.getBookById = async (req, res, next) => {
   try {
     const book_id = req.params.book_id;
-    const book = await Book.findById(book_id).populate("comments");
+    const book = await Book.findById(book_id);
+    // .populate("comments");
     res.render("user/bookDetails", { book: book });
   } catch (err) {
     console.log(err);
--- routes/books.routes.js
+++ routes/books.routes.js
@@ -7,10 +7,7 @@ const bookController = require("../controllers/books.controllers");
 // Browse books
 router.get("/", bookController.getBooks);
 
-// Fetch books by search value
-// router.post("/books/:filter/:value/:page", bookController.findBooks);
-
 // Fetch individual book details
-// router.get("/books/details/:book_id", bookController.getBookDetails);
+router.get("/:book_id", bookController.getBookById);
 
 module.exports = router;
