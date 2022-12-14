9552778cd67c7c0335741a4de2428bff996cfdfd
azad71 <mdmamun732@gmail.com>
2022-12-14 23:57:10 +0600

    added search bar

---
+++ client/src/components/searchbar/searchbar.component.jsx
@@ -0,0 +1,19 @@
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+
+function SearchBar() {
+  return (
+    <FormControl fullWidth sx={{ m: 1 }}>
+      <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+      <OutlinedInput
+        id="outlined-adornment-amount"
+        startAdornment={<InputAdornment position="start">$</InputAdornment>}
+        label="Amount"
+      />
+    </FormControl>
+  );
+}
+
+export default SearchBar;
--- client/src/pages/books/books.pages.jsx
+++ client/src/pages/books/books.pages.jsx
@@ -1,11 +1,15 @@
+import { Fragment } from "react";
 import Navbar from "../../components/navbar/navbar.component";
 import Container from "@mui/material/Container";
-import { Fragment } from "react";
+import SearchBar from "../../components/searchbar/searchbar.component";
 
 export default function BooksPage() {
   return (
     <Fragment>
       <Navbar />
+      <Container maxWidth="lg" sx={{ marginTop: "5%" }}>
+        <SearchBar />
+      </Container>
     </Fragment>
   );
 }
